2d1cc18ee94bc071598d09a30b66b2affbb83e70
Jared Acebes <jared.acebes@gmail.com>
2026-05-26 22:22:43 +0800

    chore: configuration updates for Svelte migration

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,13 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginAstro from 'eslint-plugin-astro';
-import reactPlugin from 'eslint-plugin-react';
-import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import eslintPluginSvelte from 'eslint-plugin-svelte';
 
 export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
-  {
-    // React specific config - ONLY for JSX/TSX files
-    files: ['**/*.{jsx,tsx}'],
-    plugins: {
-      react: reactPlugin,
-      'react-hooks': reactHooksPlugin,
-    },
-    rules: {
-      ...reactPlugin.configs.recommended.rules,
-      ...reactHooksPlugin.configs.recommended.rules,
-      'react-hooks/set-state-in-effect': 'off',
-      'react/react-in-jsx-scope': 'off',
-    },
-    settings: {
-      react: {
-        version: '19',
-      },
-    },
-  },
+  ...eslintPluginSvelte.configs['flat/recommended'],
   {
     ignores: ['dist/', 'node_modules/', '.astro/'],
   },
